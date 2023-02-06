0194c73172d7726576ba8445d803298947c8386f
Alexander Maltsev <aleksander.freelancer@gmail.com>
2023-02-06 09:26:29 +0600

    Stable version

--- main.py
+++
@@ -1,5 +0,0 @@
-import os
-initial_names = sorted(os.listdir("initial"))
-new_names = sorted(os.listdir("src/img"))
-for index in range(len(initial_names)):
-    os.rename("src/img/" + new_names[index], "src/img/" + initial_names[index][:-4] + ".webp")
